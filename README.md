66c560b5954f65c0c0567898a7a58ccdca60a01f
Ming Wen <bitmingw@gmail.com>
2014-07-09 07:06:42 -0700

    twitter analysis problem 1&2

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ datasci_course_materials
 
 Public repository for course materials for the Summer 2014 session of Introduction to Data Science, an online coursera course.
 
-This repository is also used to save my assignments of this course.
+This repository is only used to save my assignments of this course. Improper use prohibited.
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -9,9 +10,30 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    # hw()
+    # lines(sent_file)
+    # lines(tweet_file)
+    
+    # Build up the dictionary
+    # sent_file = open("AFINN-111.txt") # For test
+    scores = {} # empty dictionary
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score) # Convert to int
+    # print scores.items() # print the dictionary
+    
+    # Parse the data in output.txt
+    # tweet_file = open("problem_1_submission.txt") # For test
+    # tweet_file = open("output.txt") # For test
+    for line in tweet_file:
+        tweet = json.loads(line, encoding="utf-8") # tweet is a dict
+        # print tweet.keys()
+        # print tweet.values()
+        score = 0
+        for word in tweet.keys():
+            if scores.get(word) != None:
+                score += scores.get(word)
+        print score
 
 if __name__ == '__main__':
     main()
